c662755dae0df496d922563336ea724feb444053
HenriqueLimaAlves <henri633@gmail.com>
2025-11-10 17:06:46 -0300

    estrutura inicial

---
+++ src/main/java/projeto/pessoal/estudo_algoritimos/EstudoAlgoritimosApplication.java
@@ -0,0 +1,13 @@
+package projeto.pessoal.estudo_algoritimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoAlgoritimosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoAlgoritimosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/pessoal/estudo_algoritimos/EstudoAlgoritimosApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.pessoal.estudo_algoritimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoAlgoritimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
